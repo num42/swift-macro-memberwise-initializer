2eaa57d9465adef7958dc9b0904e7652f8fa8f74
Wolfgang Lutz <WLBORg@gmx.de>
2025-06-27 09:43:07 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/num42/swift-macrotester.git", from: "1.0.2"),
+    .package(url: "https://github.com/num42/swift-macrotester.git", from: "1.0.3"),
     .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.1")
   ],
   targets: [
