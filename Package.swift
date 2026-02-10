6526fef42d8d33485d6ed28f3c4e39345c598fe2
Wolfgang Lutz <wlut@num42.de>
2026-02-10 13:25:11 +0100

    fix dependency

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/num42/swift-macrohelper.git", from: "1.0.0"),
+    .package(url: "https://github.com/num42/swift-macrohelper.git", from: "1.0.1"),
     .package(url: "https://github.com/num42/swift-macrotester.git", from: "2.2.2"),
     .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0"),
   ],
